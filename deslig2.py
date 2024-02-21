e481e7af8fff54a05f9c35692e695625460cad47
cirotorres <ciro_3@hotmail.com>
2024-02-21 00:28:33 -0300

    atualizações e novas funcionalidades do app

--- deslig2.py
+++ deslig2.py
@@ -2,9 +2,22 @@ import tkinter as tk
 import threading
 import subprocess
 
-def desligar_em_5min():
-    subprocess.run(["shutdown", "/s", "/t", "300"])  
-    atualizar_cronometro()
+def desligar_em_15min():
+    subprocess.run(["shutdown", "/s", "/t", "900"])  
+    atualizar_cronometro15()
+
+def desligar_em_30min():
+    subprocess.run(["shutdown", "/s", "/t", "1800"])  
+    atualizar_cronometro30()
+
+def desligar_em_60min():
+    subprocess.run(["shutdown", "/s", "/t", "3600"])  
+    atualizar_cronometro60()
+
+def desligar_em_tempo():
+    tempo_digitado = int(entry_tempo.get()) * 60  # Converter minutos para segundos
+    subprocess.run(["shutdown", "/s", "/t", str(tempo_digitado)])  # Desliga após o tempo especificado
+    atualizar_cronometro()    
 
 def desativar_desligamento():
     subprocess.run(["shutdown", "/a"])  
@@ -13,9 +26,27 @@ def desativar_desligamento():
 def sair():
     root.destroy()
 
+def atualizar_cronometro15():
+    global tempo_restante
+    tempo_restante = 900  
+    cronometro_label.config(text=formatar_tempo(tempo_restante))
+    atualizar_cronometro_contagem()
+
+def atualizar_cronometro30():
+    global tempo_restante
+    tempo_restante = 1800  
+    cronometro_label.config(text=formatar_tempo(tempo_restante))
+    atualizar_cronometro_contagem()
+
+def atualizar_cronometro60():
+    global tempo_restante
+    tempo_restante = 3600  
+    cronometro_label.config(text=formatar_tempo(tempo_restante))
+    atualizar_cronometro_contagem()
+
 def atualizar_cronometro():
     global tempo_restante
-    tempo_restante = 300  
+    tempo_restante = int(entry_tempo.get()) * 60
     cronometro_label.config(text=formatar_tempo(tempo_restante))
     atualizar_cronometro_contagem()
 
@@ -42,10 +73,26 @@ root = tk.Tk()
 root.title("Programa de Desligamento Automático")
 
 # Definindo o tamanho da janela (largura x altura + margem_esquerda + margem_superior)
-root.geometry("400x250+300+150")
+root.geometry("400x450+300+150")
+
+
+
+label_tempo = tk.Label(root, text="Digite o tempo de desligamento (minutos):")
+label_tempo.pack(pady=10)
+
+entry_tempo = tk.Entry(root)
+entry_tempo.pack(pady=5)
+
+# Botão para desligar em tempo especificado
+btn_desligar = tk.Button(root, text="Desligar em tempo especificado", command=desligar_em_tempo)
+btn_desligar.pack(pady=10)
 
 # Botões
-btn_desligar = tk.Button(root, text="Desligar em 5 min", command=desligar_em_5min)
+btn_desligar = tk.Button(root, text="Desligar em 15 min", command=desligar_em_15min)
+btn_desligar.pack(pady=10)
+btn_desligar = tk.Button(root, text="Desligar em 30 min", command=desligar_em_30min)
+btn_desligar.pack(pady=10)
+btn_desligar = tk.Button(root, text="Desligar em 1 hora", command=desligar_em_60min)
 btn_desligar.pack(pady=10)
 
 btn_desativar = tk.Button(root, text="Desativar Desligamento", command=desativar_desligamento)
